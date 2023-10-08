d873604ee0e5ab8d4d9dd44003240ade19d88601
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-10-08 22:00:15 +0900

    Changes to be committed:

--- denops/mnh/deps.ts
+++ denops/mnh/deps.ts
@@ -1,1 +1,2 @@
 export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+export { ensureString } from "https://deno.land/x/unknownutil@v3.9.0/mod.ts"
--- denops/mnh/main.ts
+++ denops/mnh/main.ts
@@ -1,5 +1,14 @@
-import { Denops } from "./deps.ts";
+import { Denops, ensureString } from "./deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
-  console.log("Hello Denops!");
+  denops.dispatcher = {
+    async echo(text: unknown): Promise<unknown> {
+      ensureString(text);
+      return await Promise.resolve(text);
+    },
+  };
+
+  await denops.cmd(
+    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
+  );
 };
