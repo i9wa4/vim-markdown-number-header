35f0fd2279bbeaf8dc5749a2c2ddd5c1370e9db8
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-10-09 23:14:15 +0900

    Release Candidate Version.

--- denops/mnh/deps.ts
+++ denops/mnh/deps.ts
@@ -3,7 +3,3 @@ export {
   open,
   replace,
 } from "https://deno.land/x/denops_std@v5.0.1/buffer/mod.ts";
-export {
-  ensure,
-  is,
-} from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
--- denops/mnh/main.ts
+++ denops/mnh/main.ts
@@ -1,14 +1,12 @@
 import {
   Denops,
-  ensure,
-  is,
   open,
   replace,
 } from "./deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    async addNumber(): Promise<unknown> {
+    async numberHeader(): Promise<unknown> {
       // get current buffer content
       const bufnr = await denops.call("bufnr") as number;
       const content = (await denops.call(
@@ -18,26 +16,59 @@ export async function main(denops: Denops): Promise<void> {
         "$"
       )) as string[];
 
-      // add number for headings
-      // TODO: define header level shift parameter
+      // number headers
+      let secLevelShift: number = 1;
+      let secLevel: number = 0;
+      let secLevelPrev: number = 0;
       const secNumber: string[] = [0, 0, 0, 0, 0, 0];
       const contentNew: string[] = [];
-      let secDepth: number = 0;
       for (let i = 0; i < content.length; i++) {
-        if (content[i].match('^#')) {
-          contentNew[i] = "header!" + content[i]
+        // calculate the section level
+        if (content[i].match('^#{6,}')) {
+          secLevel = 6;
+        } else if (content[i].match('^#{5}')) {
+          secLevel = 5;
+        } else if (content[i].match('^#{4}')) {
+          secLevel = 4;
+        } else if (content[i].match('^#{3}')) {
+          secLevel = 3;
+        } else if (content[i].match('^#{2}')) {
+          secLevel = 2;
+        } else if (content[i].match('^#{1}')) {
+          secLevel = 1;
         } else {
-          contentNew[i] = content[i]
+          // this line is not a header
+          contentNew[i] = content[i];
+          continue;
         }
+
+        // calculate the section number
+        secNumber[secLevel - 1]++;
+        if (secLevel < secLevelPrev) {
+          secNumber.fill(0, secLevel);
+        }
+
+        // add/modify the section number
+        if (secLevel > secLevelShift) {
+          contentNew[i] =
+            "#".repeat(secLevel)
+            + " "
+            + secNumber.slice(secLevelShift, secLevel).join(".")
+            + "."
+            + content[i].replace(/^#+ *([0-9]\.)*/, "");
+        } else {
+          contentNew[i] = content[i];
+        }
+        secLevelPrev = secLevel;
       }
 
       // replace current buffer content
       await replace(denops, bufnr, contentNew);
-      return "debug";
+      return "The numbers for the headings have been added/modified.";
     },
   };
 
   await denops.cmd(
-    `command! -nargs=? AddNumber echomsg denops#request('${denops.name}', 'addNumber', [])`
+    `command! -nargs=? NumberHeader echomsg denops#request('${denops.name}', 'numberHeader', [])`
   );
 };
